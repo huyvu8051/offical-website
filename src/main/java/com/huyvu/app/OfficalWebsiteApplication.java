bd6345f73f0684caf9f9ad104f9504bad091857c
huyvu <huyvu8051@gmail.com>
2021-07-29 23:31:14 +0700

    init Spring boot

---
+++ src/main/java/com/huyvu/app/OfficalWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.huyvu.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficalWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficalWebsiteApplication.class, args);
+	}
+
+}
